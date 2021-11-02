039450e4f870880d5f3eb30e8d0cc20c62a98244
daouda <daoudasidibe224@gmail.com>
2021-11-02 17:26:44 +0100

    WT CRUD

--- client/src/components/Workingtimes.vue
+++ client/src/components/Workingtimes.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
     <h1>Working times</h1>
+    <form>
+      <div class="form-group">
+        <label for="start">Date de début:</label>
+        <input type="datetime-local" id="start" name="start" v-model="start" />
+      </div>
+      <div class="form-group">
+        <label for="end">Date de fin:</label>
+        <input type="datetime-local" id="end" name="end" v-model="end" />
+      </div>
+      <div class="form-group">
+        <input class="btn btn-primary mr-1" type="button" value="Rechercher" />
+      </div>
+    </form>
     <table>
       <tr>
         <td>Début</td>
@@ -10,7 +23,13 @@
       <tr v-for="workingtime in workingtimes" :key="workingtime.id">
         <td>{{ workingtime.start }}</td>
         <td>{{ workingtime.end }}</td>
-        <router-link :to="{ name: 'WorkingtimeEdit', query:{userId: userId, workingtimeId : workingtime.id} }">Editer</router-link>
+        <router-link
+          :to="{
+            name: 'WorkingtimeEdit',
+            query: { userId: userId, workingtimeId: workingtime.id },
+          }"
+          >Editer</router-link
+        >
       </tr>
     </table>
   </div>
@@ -19,17 +38,27 @@
 <script>
 import { useStore } from "vuex";
 import { computed } from "vue";
+import moment from "moment";
 
 export default {
   name: "Workingtimes",
   setup() {
     const store = useStore();
-    const user  = computed(() => store.getters.getUser);
-    store.dispatch("loadWorkingtimes", user.value.id);
+    const user = computed(() => store.getters.getUser);
+
+    const start = moment().subtract(29, "days").format("YYYY-MM-DD hh:mm:ss");
+    const end = moment().add(1, "days").format("YYYY-MM-DD hh:mm:ss");
     const userId = user.value.id;
+    const wtData = {
+      userId,
+      start,
+      end,
+    };
+
+    store.dispatch("loadWorkingtimes", wtData);
     const workingtimes = computed(() => store.getters.getWorkingtimes);
 
-    return { workingtimes, userId };
+    return { workingtimes, userId, start, end };
   },
 };
 </script>
