25b198b3ff0d4e335432c29cf47245594e5b82c7
paul.tardivel@epitech.eu <paul.tardivel@epitech.eu>
2021-11-02 14:18:28 +0100

    add deps

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,7 +1,4 @@
-# entrypoint.sh
-
-#!/bin/bash
-# Docker entrypoint script.
+#!/usr/bin/env bash
 
 if [ -f ".env" ]; then
     echo "$FILE exists."
@@ -10,21 +7,16 @@ else
     exit 1
 fi
 
-# Wait until Postgres is ready
-echo "TEST ENTRYPOINT"
-while ! pg_isready -q -h $PGHOST -p $PGPORT -U $PGUSER
+while ! pg_isready -q -h $PGHOST -p $PGPORT --username=$PGUSER
 do
-  echo "$(date) - waiting for database to start"
   sleep 2
 done
 
-# Create, migrate, and seed database if it doesn't exist.
-if [[ -z `psql -Atqc "\\list $PGDATABASE"` ]]; then
-  echo "Database $PGDATABASE does not exist. Creating..."
+if [[ -z `psql -qc "\\l $PGDATABASE"` ]]; then
   createdb -E UTF8 $PGDATABASE -l en_US.UTF-8 -T template0
   mix ecto.migrate
   mix run priv/repo/seeds.exs
-  echo "Database $PGDATABASE created."
+  echo "The database $PGDATABASE has been created."
 fi
 
 exec mix phx.server
